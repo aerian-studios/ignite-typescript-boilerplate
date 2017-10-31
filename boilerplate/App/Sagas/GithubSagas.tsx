85f8afdccd64230367b8a86edc706bf3eea169a7
Matt Kane <ascorbic@gmail.com>
2017-10-31 10:48:54 +0000

    Use typed states

--- boilerplate/App/Sagas/GithubSagas.tsx
+++ boilerplate/App/Sagas/GithubSagas.tsx
@@ -1,14 +1,16 @@
+import { ApiResponse } from "apisauce";
 import { path } from "ramda";
 import { call, put } from "redux-saga/effects";
-import GithubActions from "../Redux/GithubRedux";
+import { GithubAction, GithubActions } from "../Redux/GithubRedux";
+import { GithubApi, GithubResponse, GithubUser } from "../Services/Api";
 
-export function * getUserAvatar(api, action) {
+export function * getUserAvatar(api: GithubApi, action: GithubAction) {
   const { username } = action;
   // make the call to the api
-  const response = yield call(api.getUser, username);
+  const response: ApiResponse<GithubResponse> = yield call(api.getUser, username);
 
   if (response.ok) {
-    const firstUser = path(["data", "items"], response)[0];
+    const firstUser = path<GithubUser[]>(["data", "items"], response)[0];
     const avatar = firstUser.avatar_url;
     // do data conversion here if needed
     yield put(GithubActions.userSuccess(avatar));
