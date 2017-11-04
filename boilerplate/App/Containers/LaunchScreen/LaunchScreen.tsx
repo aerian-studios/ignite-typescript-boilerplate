0fa46e89a6cbf98b35c4b70b1c4e96dd15bdcb35
ascorbic <ascorbic@gmail.com>
2017-11-04 09:51:40 +0000

    tyop

--- boilerplate/App/Containers/LaunchScreen/LaunchScreen.tsx
+++ boilerplate/App/Containers/LaunchScreen/LaunchScreen.tsx
@@ -20,7 +20,7 @@ export default class LaunchScreen extends Component {
           <View style={styles.section} >
             <Image source={Images.ready} />
             <Text style={styles.sectionText}>
-              This probably is what your app is going to look like. Unless your designer
+              This probably isn't what your app is going to look like. Unless your designer
               handed you this screen and, in that case, congrats! You're ready to ship.
               For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
